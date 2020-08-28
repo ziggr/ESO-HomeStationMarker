9b4180a8497ea661423f8867d9d6866aa66618b6
Zig Zichterman <github@ziggr.com>
2020-08-28 16:02:13 -0700

    RU translation by DrakeRulo

---
+++ lang/ru.lua
@@ -0,0 +1,37 @@
+HomeStationMarker.LANG = HomeStationMarker.LANG or {}
+HomeStationMarker.LANG["ru"] = {
+  ["interact"] = {
+    ["BANKER"    ] = "Tythis Andromo, the Banker"
+  , ["BANKER.2"  ] = "Ezabi the Banker"
+  , ["MERCHANT"  ] = "Nuzhimeh the Merchant"
+  , ["MERCHANT.2"] = "Fezez the Merchant"
+  , ["FENCE"     ] = "Pirharri the Smuggler"
+
+  , ["APPRENTICE"] = "Ученик"
+  , ["ATRONACH"  ] = "Атронах"
+  , ["LADY"      ] = "Леди"
+  , ["LORD"      ] = "Лорд"
+  , ["LOVER"     ] = "Любовник"
+  , ["MAGE"      ] = "Маг"
+  , ["RITUAL"    ] = "Ритуал"
+  , ["SERPENT"   ] = "Змей"
+  , ["SHADOW"    ] = "Тень"
+  , ["STEED"     ] = "Конь"
+  , ["THIEF"     ] = "Вор"
+  , ["TOWER"     ] = "Башня"
+  , ["WARRIOR"   ] = "Воин"
+  },
+  ["slash_commands"] = {
+    ["SC_SET"                 ] = "set"
+  , ["SC_STATION"             ] = "station"
+  , ["SC_SET_STATION"         ] = "%s <%s> <%s>"
+  , ["SC_FORGET_LOCS"         ] = "Забыть расположение всех станций, также стереть все маркеры для текущего дома."
+  , ["SC_FORGET_LOCS_CMD"     ] = "forgetlocs"
+  , ["SC_FORGET_LOCS_ALL"     ] = "Забыть расположение всех станций для всех домов, также стереть все маркеры."
+  , ["SC_FORGET_LOCS_ALL_CMD" ] = "forgetlocs_all"
+  , ["SC_SCAN_LOCS"           ] = "Сканировать, чтобы изучить расположение станций."
+  , ["SC_SCAN_LOCS_CMD"       ] = "scanlocs"
+  , ["SC_CLEAR_MARKS"         ] = "Убрать все маркеры."
+  , ["SC_CLEAR_MARKS_CMD"     ] = "clear"
+  },
+}
